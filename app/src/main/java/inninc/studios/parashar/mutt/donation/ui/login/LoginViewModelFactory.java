402591a8d7e0b6662b81d752028e2755d65c863c
nks102000 <nitishks1110@gmail.com>
2021-01-12 19:50:47 +0530

    inisial commit

---
+++ app/src/main/java/inninc/studios/parashar/mutt/donation/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package inninc.studios.parashar.mutt.donation.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import inninc.studios.parashar.mutt.donation.data.LoginDataSource;
+import inninc.studios.parashar.mutt.donation.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
