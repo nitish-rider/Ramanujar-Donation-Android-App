cf899d41ddf386b26a98b3a462ea7b4533114c1d
nks102000 <nitishks1110@gmail.com>
2021-01-12 20:38:19 +0530

    login activity commit

---
+++ app/src/main/java/inninc/studios/parashar/mutt/donation/login.java
@@ -0,0 +1,45 @@
+package inninc.studios.parashar.mutt.donation;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText password = (EditText)findViewById(R.id.password);
+        Button log=(Button)findViewById(R.id.login);
+
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin-donation") && password.getText().toString().equals("ramanujar-donation")){
+                    Intent intent=new Intent(login.this,MainActivity.class);
+                    startActivity(intent);
+                }else {
+                    AlertDialog dil=new AlertDialog.Builder(login.this).setTitle("Error").setMessage("Login Faild")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int i) {
+                                    dialog.dismiss();
+                                }
+                            }).create();
+                    dil.show();
+                }
+            }
+        });
+
+    }
+}
