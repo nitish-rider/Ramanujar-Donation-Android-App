d633ba269bfe644815666e8e1f7049683967d73b
nks102000 <nitishks1110@gmail.com>
2021-01-16 10:53:22 +0530

    pdf activity

--- app/src/main/java/inninc/studios/parashar/mutt/donation/Form.java
+++ app/src/main/java/inninc/studios/parashar/mutt/donation/Form.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Canvas;
---
+++ app/src/main/java/inninc/studios/parashar/mutt/donation/PDF_activity.java
@@ -0,0 +1,14 @@
+package inninc.studios.parashar.mutt.donation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PDF_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_d_f_activity);
+    }
+}
